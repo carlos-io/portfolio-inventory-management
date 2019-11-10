dbfebbb9351af0d5f58031893ca7375bc02bdef1
Carlos Velazquez <carlosjvelazquez@gmail.com>
2019-11-09 23:32:17 -0500

    wire up categories with data and behavior

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -1,32 +1,82 @@
 'use strict'
+const Database = use('Database')
+const escape = use('sqlstring').escape
 
 class CategoryController {
-    index({view}) {
-        return view.render('admin/categories/all')
+    async index({view}) {
+        try {
+            let categories = await Database.raw(`
+                SELECT c.id, c.name,
+                       CONCAT(u.f_name, ' ', u.l_name) AS user,
+                       DATE_FORMAT(c.updated_at, '%W %b %d %Y %h:%i %p') AS updated_at
+                FROM inventory.categories c
+                INNER JOIN inventory.users u
+                ON c.users_id = u.id
+                ORDER BY c.name ASC
+            `)
+            categories = categories[0]
+            return view.render('/admin/categories/all', {categories})
+        } catch (error) {
+            return view.render('/admin/error', {error})
+        }
     }
 
-    store({view, request, response}) {
-        return response.redirect('/admin/categories')
+    async store({view, request, response}) {
+        try {
+            const category = request.post()
+            await Database.raw(`
+                INSERT INTO inventory.categories (name, users_id)
+                VALUES (${escape(category.name)}, 1)
+            `)
+            return response.redirect('/admin/categories')
+        } catch (error) {
+            return view.render('/admin/error', {error})
+        }
     }
 
     create({view}) {
-        return view.render('admin/categories/create')
+        return view.render('/admin/categories/create')
     }
 
-    show({view, request, response, params}) {
+    async edit({view, params}) {
+        try {
+            let category = await Database.raw(`
+                SELECT id, name, users_id
+                FROM inventory.categories
+                WHERE id = ${parseInt(params.id)}
+            `)
+            category = category[0][0]
+            return view.render('/admin/categories/edit', {category})
+        } catch (error) {
+            return view.render('/admin/error', {error})
+        }
 
     }
 
-    edit({view, request, response, params}) {
-
+    async update({view, request, response, params}) {
+        try {
+            const category = request.post()
+            await Database.raw(`
+                UPDATE inventory.categories
+                SET name = ${escape(category.name)}
+                WHERE id = ${parseInt(params.id)}
+            `)
+            return response.redirect('/admin/categories')
+        } catch (error) {
+            return view.render('/admin/error', {error})
+        }
     }
 
-    edit({view, request, response, params}) {
-
-    }
-
-    delete({view, request, response, params}) {
-
+    async delete({view, response, params}) {
+        try {
+            await Database.raw(`
+                DELETE FROM inventory.categories
+                WHERE id = ${parseInt(params.id)}
+            `)
+            return response.redirect('/admin/categories')
+        } catch (error) {
+            return view.render('/admin/error', {error})
+        }
     }
 }
 
